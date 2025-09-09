ca8c7e00c306b8e5bcda2823c4ee3c11edd8b166
Lavanyadanda <157560770+Lavanyadanda@users.noreply.github.com>
2025-09-09 16:58:58 +0530

    Update ProductCard.jsx

--- product-frontend/src/components/ProductCard.jsx
+++ product-frontend/src/components/ProductCard.jsx
@@ -1,34 +1,3 @@
-// import React from "react";
-
-// function ProductCard({ product, onDelete }) {
-//   const handleDelete = () => {
-//     if (window.confirm("Delete this product?")) {
-//       onDelete(product._id);
-//     }
-//   };
-
-//   return (
-//     <div className="product-card">
-//       <h3>{product.name}</h3>
-//       <p>
-//         <strong>Price:</strong> Rs.{product.price}
-//       </p>
-    
-//       <p>{product.description}</p>
-      
-//       <p>
-//         <em>{product.category}</em>
-//       </p>
-      
-//       <button className="delete-btn" onClick={handleDelete}>
-//         Delete
-//       </button>
-//     </div>
-//   );
-// }
-
-// export default ProductCard;
-
 import React, { useState } from "react";
 
 function ProductCard({ product, onDelete, onUpdate }) {
